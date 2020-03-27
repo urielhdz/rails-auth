54aecd372f77909a420fe2ef049902a2e27e7835
urielhdz <uriel.hdzc@gmail.com>
2020-03-27 11:10:51 -0600

    Tuto!

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,34 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// Shannon entropy
+
+  // entropy.js MIT License © 2014 James Abney http://github.com/jabney
+  // ES6 portation MIT License © 2017 Peter Seprus http://github.com/ppseprus
+
+  // Calculate the Shannon entropy of a string in bits per symbol.
+  (function (shannon) {
+    'use strict';
+
+    // Create an array of character frequencies.
+    const getFrequencies = str => {
+      let dict = new Set(str);
+      return [...dict].map(chr => {
+        return str.match(new RegExp(chr, 'g')).length;
+      });
+    };
+
+    // Measure the entropy of a string in bits per symbol.
+    shannon.entropy = str => getFrequencies(str)
+      .reduce((sum, frequency) => {
+        let p = frequency / str.length;
+        return sum - (p * Math.log(p) / Math.log(2));
+      }, 0);
+
+    // Measure the entropy of a string in total bits.
+    shannon.bits = str => shannon.entropy(str) * str.length;
+
+    // Log the entropy of a string to the console.
+    shannon.log = str => console.log(`Entropy of "${str}" in bits per symbol:`, shannon.entropy(str));
+
+  })(window.shannon = window.shannon || Object.create(null));
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :password, password_strength: true
 end
